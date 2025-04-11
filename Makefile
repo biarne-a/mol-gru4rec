2c0cefbbf64d33f16b2837bacc9261e876ab8508
Adrien Biarnes <adrien.biarnes@shopify.com>
2025-04-11 15:43:15 -0400

    Install remote

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 setup_preprocess:
-    mv pyproject.toml _pyproject.toml
-    mv pyproject_preprocess.toml pyproject.toml
-    UV_PROJECT_ENVIRONMENT=.venv_preprocess uv sync
-    mv pyproject.toml pyproject_preprocess.toml
-    mv _pyproject.toml pyproject.toml
+	mv pyproject.toml _pyproject.toml
+	mv pyproject_preprocess.toml pyproject.toml
+	UV_PROJECT_ENVIRONMENT=.venv_preprocess uv sync
+	mv pyproject.toml pyproject_preprocess.toml
+	mv _pyproject.toml pyproject.toml
 
 setup:
 	uv sync
---
+++ install_remote.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+git clone https://github.com/biarne-a/mol-gru4rec.git
+cd mol-gru4rec/
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+
+
+mkdir -p data/parquets
+gsutil -m cp -r gs://movie-lens-25m/parquets/gru4rec_ml1m_full_slide data/parquets/
+gsutil -m cp -r gs://movie-lens-25m/vocab data/
+mkdir logs
+uv sync
+source .venv/bin/activate
+python -m train --data_dir data --config_name gru4rec_config_ml-1m_64_mol_uid --dataset_name gru4rec_ml1m_full_slide | tee logs/gru4rec_config_ml-1m_64_mol_uid.logs
