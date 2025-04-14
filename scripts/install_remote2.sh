c662c538c8261a6559df0454b6bba05c07b6b54c
Adrien Biarnes <adrien.biarnes@shopify.com>
2025-04-14 14:40:29 -0400

    Split install.sh

---
+++ scripts/install_remote2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcloud auth login --update-adc
+gcloud config set project concise-haven-277809
---
+++ scripts/install_remote3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd mol-gru4rec/
+mkdir -p data/parquets
+gsutil -m cp -r gs://movie-lens-25m/ml-1m data/
+gsutil -m cp -r gs://movie-lens-25m/vocab data/
+gsutil -m cp -r gs://movie-lens-25m/parquets/gru4rec_ml1m_full_slide data/parquets/
+uv sync
+source .venv/bin/activate
+python -m train --data_dir data --config_name gru4rec_config_ml-1m_64_mol_uid_semantic_embed --dataset_name gru4rec_ml1m_full_slide
